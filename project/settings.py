e278a1989008a37242bb97e8a2673a2fb6cb219d
Reuben <reubenabrahams77@gmail.com>
2025-08-30 19:59:53 +0200

    fix(settings): correct STATICFILES_DIRS spelling to load static files

--- project/settings.py
+++ project/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRs = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Auth redirects
