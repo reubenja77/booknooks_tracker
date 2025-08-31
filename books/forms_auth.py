39e687a53f71bbffbfc24ce70654665c3fb78fe4
Reuben <reubenabrahams77@gmail.com>
2025-08-31 14:54:29 +0200

    fix(auth): Import get_user_model for BootstrapUserCreationForm

--- books/forms_auth.py
+++ books/forms_auth.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import get_user_model
 
 class BootstrapAuthenticationForm(AuthenticationForm):
     username = forms.CharField(
@@ -12,3 +13,34 @@ class BootstrapAuthenticationForm(AuthenticationForm):
         widget=forms.PasswordInput(attrs={"class": "form-control",
             "autocomplete": "current-password",})
     )
+
+class BootstrapUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=False, widget=forms.EmailInput(attrs={
+        "class": "form-control",
+        "autocomplete": "email",
+    }))
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "form-control",
+        "autocomplete": "username",
+        "autocapitalize": "none",
+    }))
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={
+        "class": "form-control",
+        "autocomplete": "new-password",
+    }))
+    password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput(attrs={
+        "class": "form-control",
+        "autocomplete": "new-password",
+    }))
+
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+        fields = ("username", "email")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data.get("email", "")
+        if commit:
+            user.save()
+        return user
